c1a16beaebf151a2ba49a395594a70b6986100d9
xnki7 <codeinja7@gmail.com>
2023-08-28 21:22:33 +0530

    initialised project with components and pages

---
+++ src/Pages/PostDetail/PostDetail.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./PostDetail.css"
+
+const PostDetail = () => {
+    return (
+        <div className='PostDetail'>PostDetail</div>
+    )
+}
+
+export default PostDetail
---
+++ src/Pages/SavedPosts/SavedPosts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./SavedPosts.css"
+
+const SavedPosts = () => {
+    return (
+        <div className='SavedPosts'>SavedPosts</div>
+    )
+}
+
+export default SavedPosts
