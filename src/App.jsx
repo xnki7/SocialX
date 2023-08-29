deff11067444d10bb908e7c1b809d19521d8c24e
xnki7 <codeinja7@gmail.com>
2023-08-29 14:28:14 +0530

    Connect Wallet done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import './App.css'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { sepolia } from 'wagmi/chains'
+import ConnectWallet from './Pages/ConnectWallet/ConnectWallet'
+import { useState } from 'react'
+
+const chains = [sepolia]
+const projectId = '641b78ab05eb8ddcdc68fc15be6cd561'
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [accountAddress, setAccountAddress] = useState(null);
   return (
-    <div className="App">
-      <h1>Hello man</h1>
-    </div>
+    <>
+      <WagmiConfig config={wagmiConfig}>
+        <ConnectWallet setIsConnected={setIsConnected} setAccountAddress={setAccountAddress} />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
   )
 }
-
 export default App
---
+++ src/Pages/ConnectWallet/ConnectWallet.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Web3Button } from '@web3modal/react'
+import { useAccount } from 'wagmi'
+import "./ConnectWallet.css"
+import logo from "./SocialX.png"
+
+const ConnectWallet = ({ setIsConnected, setAccountAddress }) => {
+    const account = useAccount({
+        onConnect({ address }) {
+            setIsConnected(true);
+            setAccountAddress(address);
+            console.log('Connected', { address });
+        },
+    })
+    return <div className="ConnectWallet">
+        <img src={logo} />
+        <Web3Button />
+        <p className="footer">Made Just for You, Filled with ❤️</p>
+    </div>
+}
+
+export default ConnectWallet
