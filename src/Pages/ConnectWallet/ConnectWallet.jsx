432dd3f500d8fd451e7f644a6ff6e593e8670811
xnki7 <codeinja7@gmail.com>
2023-08-29 19:32:46 +0530

    Bug fix1

--- src/Pages/ConnectWallet/ConnectWallet.jsx
+++ src/Pages/ConnectWallet/ConnectWallet.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Web3Button } from '@web3modal/react'
 import { useAccount } from 'wagmi'
+import { useNavigate } from "react-router-dom";
 import "./ConnectWallet.css"
 import logo from "./SocialX.png"
 
 const ConnectWallet = ({ setIsConnected, setAccountAddress }) => {
+    const navigate = useNavigate();
     const account = useAccount({
         onConnect({ address }) {
             setIsConnected(true);
             setAccountAddress(address);
             console.log('Connected', { address });
+            navigate('/createprofile')
         },
     })
     return <div className="ConnectWallet">
