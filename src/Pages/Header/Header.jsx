20ec84f20717c273bc8274663a164a2dde391bd4
Ankit Kumar <98731902+ankit-kr27@users.noreply.github.com>
2023-09-05 17:01:40 +0530

    New Header Component

---
+++ src/Pages/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import "./Header.css"
+export default function Header(){
+    return (
+        <div className='headerContainer'>
+            <img className="logoImg" src="Images/SocialX.svg" alt="" />
+            <Link to='/savedposts'>
+                <button className='postBtns'><img src="Images/SavedPosts.svg" alt="" /></button>
+            </Link>
+        </div>
+    )
+}
