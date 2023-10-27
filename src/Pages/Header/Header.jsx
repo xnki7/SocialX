753f37dc48d59c77530c77da4bbba9f6be58ea12
xnki7 <codeinja7@gmail.com>
2023-10-27 13:55:05 +0530

    Css logo

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom';
+import img from "./SocialX.svg"
+import save from "./SavedPosts.svg"
 import "./Header.css"
+import log1 from "./log1.svg"
+
 export default function Header() {
     return (
         <div className='headerContainer'>
-            <img className="logoImg" src="Images/SocialX.svg" alt="" />
+            <Link to='/homepage'>
+                <img className='logoo' src={log1} alt="" width="34px" height="34px" />
+            </Link>
+            <img className="logoImg" src={img} alt="" />
             <Link to='/savedposts'>
-                <button className='postBtns'><img src="Images/SavedPosts.svg" alt="" /></button>
+                <button className='postBtns'><img src={save} alt="" /></button>
             </Link>
         </div>
     )
-}
+} 
